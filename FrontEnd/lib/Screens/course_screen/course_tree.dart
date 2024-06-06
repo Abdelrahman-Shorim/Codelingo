759c72f9e68a080c69eff9e225b00a5407bb9e67
HabibaMohamed07 <Habiba2003520@miuegypt.edu.eg>
2024-06-07 01:07:14 +0300

    coursetree modifications

--- FrontEnd/lib/Screens/course_screen/course_tree.dart
+++ FrontEnd/lib/Screens/course_screen/course_tree.dart
@@ -5,127 +5,307 @@ import 'components/double_course_node.dart';
 import 'components/triple_course_node.dart';
 
 class CourseTree extends StatelessWidget {
-  const CourseTree({Key? key}) : super(key: key);
+  // const CourseTree({Key? key}) : super(key: key);
+
+  // Define a list of colors for unit containers
+  final List<Color> unitColors = [
+    Color.fromARGB(255, 42, 230, 155),
+    Color.fromARGB(255, 255, 175, 197),
+    Color.fromARGB(255, 224, 71, 158),
+    Colors.yellow,
+    Colors.orange,
+    Colors.purple,
+    Colors.teal,
+    Colors.indigo,
+    Colors.amber,
+    Colors.cyan,
+    Colors.lime,
+  ];
+
+  // Build function for each unit
+  Widget buildUnit(BuildContext context, String unitName, String unitDescription, List<String> nodeTitles, Color color) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          color: color, // Assign color to the unit container
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    unitName,
+                    style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    unitDescription,
+                    style: const TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: const Icon(Icons.list, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+        const Padding(padding: EdgeInsets.all(10)),
+        CourseNode(nodeTitles[0], color: color, crown: 1), // First node title
+        const Padding(padding: EdgeInsets.all(10)),
+        DoubleCourseNode(
+          CourseNode(nodeTitles[1], image: 'assets/images/hand.png', color: color, percent: 0.5, crown: 1),
+          CourseNode(nodeTitles[2], image: 'assets/images/pen.png', color: color, crown: 2),
+        ),
+        const Padding(padding: EdgeInsets.all(10)),
+        DoubleCourseNode(
+          CourseNode(nodeTitles[3], image: 'assets/images/fish.png', color: color, percent: 0.2, crown: 3),
+          CourseNode(nodeTitles[4], image: 'assets/images/bucket.png', color: color, crown: 1),
+        ),
+        const Padding(padding: EdgeInsets.all(10)),
+        CourseNode(nodeTitles[5], image: 'assets/images/bandages.png', color: color, crown: 4), // Last node title
+        const Padding(padding: EdgeInsets.all(10)),
+        // TripleCourseNode(
+        //   CourseNode(nodeTitles[6], image: 'assets/images/heel.png', color: color),
+        //   CourseNode(nodeTitles[7], image: 'assets/images/hammer.png', color: color, percent: 0.7),
+        //   CourseNode(nodeTitles[8], image: 'assets/images/emotion.png', color: color, crown: 2),
+        // ),
+        // const Padding(padding: EdgeInsets.all(10)),
+        // DoubleCourseNode(
+        //   CourseNode(nodeTitles[9], image: 'assets/images/dumbbell.png', color: color, percent: 0.2),
+        //   CourseNode(nodeTitles[10], image: 'assets/images/tree.png', color: color),
+        // ),
+        const Padding(padding: EdgeInsets.only(bottom: 15)),
+      ],
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
-          const Padding(padding: EdgeInsets.all(10)),
-          // Unit Information
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            color: Colors.deepPurple[300],
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Unit 1',
-                      style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Introduction to C++',
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: const Icon(Icons.list, color: Colors.white),
-                ),
-              ],
-            ),
+          buildUnit(
+            context,
+            'Unit 1',
+            'Introduction to C++',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[0],
           ),
-          const Padding(padding: EdgeInsets.all(10)),
-          // Course Nodes
-          CourseNode(
-            'Intro to C++',
-            crown: 1,
+          buildUnit(
+            context,
+            'Unit 2',
+            'Expressions',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[1],
           ),
-          const Padding(padding: EdgeInsets.all(10)),
-          DoubleCourseNode(
-            CourseNode(
-              'Expressions',
-              image: 'assets/images/hand.png',
-              color: Colors.yellow,
-              percent: 0.5,
-              crown: 1,
-            ),
-            CourseNode(
-              'If-Statements',
-              image: 'assets/images/pen.png',
-              color: const Color(0xFFCE82FF),
-              crown: 2,
-            ),
+                      buildUnit(
+            context,
+            'Unit 3',
+            'If-statements',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[2],
           ),
-          const Padding(padding: EdgeInsets.all(10)),
-          DoubleCourseNode(
-            CourseNode(
-              'Switch cases',
-              image: 'assets/images/fish.png',
-              color: Colors.green,
-              percent: 0.2,
-              crown: 3,
-            ),
-            CourseNode(
-              'For Loops',
-              image: 'assets/images/bucket.png',
-              color: Colors.blue,
-              crown: 1,
-            ),
+          buildUnit(
+            context,
+            'Unit 4',
+            'Switch Cases',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[3],
           ),
-          const Padding(padding: EdgeInsets.all(10)),
-          CourseNode(
+          buildUnit(
+            context,
+            'Unit 5',
+            'For Loops',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[4],
+          ),
+          buildUnit(
+            context,
+            'Unit 6',
             'While Loops',
-            image: 'assets/images/bandages.png',
-            color: Colors.redAccent,
-            crown: 4,
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[5],
           ),
-          const Padding(padding: EdgeInsets.all(10)),
-          TripleCourseNode(
-            CourseNode(
-              'Do-While',
-              image: 'assets/images/heel.png',
-              color: Colors.yellow.shade500,
-            ),
-            CourseNode(
-              'Type Casting',
-              image: 'assets/images/hammer.png',
-              color: Colors.grey,
-              percent: 0.7,
-            ),
-            CourseNode(
-              'Nested Loops',
-              image: 'assets/images/emotion.png',
-              color: Colors.blue,
-              crown: 2,
-            ),
+          buildUnit(
+            context,
+            'Unit 7',
+            'Do-While',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[6],
           ),
-          const Padding(padding: EdgeInsets.all(10)),
-          DoubleCourseNode(
-            CourseNode(
-              'Mathematical Functions',
-              image: 'assets/images/dumbbell.png',
-              color: Colors.purple.shade200,
-              percent: 0.2,
-            ),
-            CourseNode(
-              'Introduction \n to \n functions',
-              image: 'assets/images/tree.png',
-              color: Colors.pink,
-            ),
+          buildUnit(
+            context,
+            'Unit 8',
+            'Type Casting',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[7],
           ),
-          const Padding(padding: EdgeInsets.only(bottom: 15)),
+          buildUnit(
+            context,
+            'Unit 9',
+            'Nested Loops',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[8],
+          ),
+          buildUnit(
+            context,
+            'Unit 10',
+            'Mathematical Functions',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+            ],
+            unitColors[9],
+          ),
+          buildUnit(
+            context,
+            'Unit 11',
+            'Into to functions',
+            [
+              'Lesson 1', 
+              'Lesson 2', 
+              'Lesson 3', 
+              'Lesson 4', 
+              'Lesson 5', 
+              'Lesson 6', 
+              'Lesson 7', 
+              'Lesson 8', 
+              'Lesson 9', 
+              'Lesson 10', 
+              'Lesson 11', 
+              
+
+            ],
+            unitColors[10],
+          ),
         ],
       ),
     );
   }
 }
+
