3157204a28b41b13e588fba46593b1cadac59cb9
Habiba1234567 <habiba2008121@miuegypt.edu.eg>
2024-06-09 01:39:53 +0300

    Added course form

---
+++ codelingo/lib/Forms/Course.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class CourseForm extends StatefulWidget {
+  const CourseForm({super.key});
+
+  @override
+  State<CourseForm> createState() => _CourseFormState();
+}
+
+class _CourseFormState extends State<CourseForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Add course",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Name",
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.green.withOpacity(0.5), width: 1.0),
+                  ),
+                ),
+                cursorColor: Colors.green,
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return "Name required";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Description",
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.green.withOpacity(0.5), width: 1.0),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return "Description required";
+                  }
+                  return null;
+                },
+                cursorColor: Colors.green,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Number of Units",
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Colors.green.withOpacity(0.5), width: 1.0),
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    return "Number of units required";
+                  }
+                  return null;
+                },
+                cursorColor: Colors.green,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.green,
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
