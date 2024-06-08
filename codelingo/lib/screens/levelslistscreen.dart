4e7c1f08bbd5de1277dd946d1da8fe1a8a704547
Abdelrahman-Shorim <abdelrahmanshorim@gmail.com>
2024-06-08 18:13:55 +0300

    removed unnecessary screens

--- codelingo/lib/screens/levelslistscreen.dart
+++ codelingo/lib/screens/levelslistscreen.dart
@@ -1,43 +1,101 @@
-<<<<<<< HEAD
-// import 'package:codelingo/models/UnitLevelsModel.dart';
+// <<<<<<< HEAD
+// // import 'package:codelingo/models/UnitLevelsModel.dart';
+// // import 'package:flutter/material.dart';
+// // // import 'package:codelingo/models/LevelsModel.dart';
+// // import 'package:codelingo/models/TopicsModel.dart';
+// // import 'package:codelingo/LevelsService.dart';
+
+// // class LevelsScreen extends StatefulWidget {
+// //   @override
+// //   _LevelsScreenState createState() => _LevelsScreenState();
+// // }
+// =======
+// import 'package:codelingo/models/LevelsModel.dart';
 // import 'package:flutter/material.dart';
 // // import 'package:codelingo/models/LevelsModel.dart';
-// import 'package:codelingo/models/TopicsModel.dart';
-// import 'package:codelingo/LevelsService.dart';
+// import 'package:codelingo/services/LevelsService.dart';
 
 // class LevelsScreen extends StatefulWidget {
+//   const LevelsScreen({super.key});
+
 //   @override
 //   _LevelsScreenState createState() => _LevelsScreenState();
 // }
-=======
-import 'package:codelingo/models/LevelsModel.dart';
-import 'package:flutter/material.dart';
-// import 'package:codelingo/models/LevelsModel.dart';
-import 'package:codelingo/services/LevelsService.dart';
+// >>>>>>> 46dfe9e0f8ab133194e4615cc29ac3585a8d5804
 
-class LevelsScreen extends StatefulWidget {
-  const LevelsScreen({super.key});
+// // class _LevelsScreenState extends State<LevelsScreen> {
+// //   final LevelsService _levelsService = LevelsService();
+// //   List<UnitLevelsModel> _levels = [];
 
-  @override
-  _LevelsScreenState createState() => _LevelsScreenState();
-}
->>>>>>> 46dfe9e0f8ab133194e4615cc29ac3585a8d5804
+// //   bool _isLoading = false;
 
-// class _LevelsScreenState extends State<LevelsScreen> {
-//   final LevelsService _levelsService = LevelsService();
-//   List<UnitLevelsModel> _levels = [];
-
-//   bool _isLoading = false;
-
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text('Levels'),
+// //         actions: [
+// //           IconButton(
+// //             icon: Icon(Icons.add),
+// //             onPressed: () {
+// //               // Add predefined data to levels
+// //               _addPredefinedData();
+// //             },
+// //           ),
+// //         ],
+// //       ),
+// //       body:
+// //           // StreamBuilder<List<LevelsModel>>(
+// //           // stream: _levelsService.getAllLevelss(),
+// //           // builder: (context, snapshot) {
+// //           //   if (snapshot.connectionState == ConnectionState.waiting) {
+// //           //     return Center(child: CircularProgressIndicator());
+// //           //   } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+// //           //     return Center(child: Text('No levels to show'));
+// //           //   } else {
+// //           // return
+// //           Center(
+// //         child: _isLoading
+// //             ? CircularProgressIndicator()
+// //             : _levels.isEmpty
+// //                 ? Text('No Levels yet')
+// //                 : ListView.builder(
+// //                     itemCount: _levels.length,
+// //                     itemBuilder: (context, index) {
+// //                       UnitLevelsModel level = _levels[index];
+// //                       return ListTile(
+// //                         title: Text(level.name),
+// //                         subtitle: level.topicsuid.isNotEmpty
+// //                             ? Column(
+// //                                 crossAxisAlignment: CrossAxisAlignment.start,
+// //                                 children: level.topicsuid
+// //                                     .map((topic) => Text(topic))
+// //                                     .toList(),
+// //                               )
+// //                             : Text('No topics'),
+// //                       );
+// //                     },
+// //                   ),
+// //       ),
+// //       floatingActionButton: FloatingActionButton(
+// //         onPressed: _fetchUsers,
+// //         child: Icon(Icons.refresh),
+// //       ),
+// //       // }
+// //       // },
+// //       // ),
+// //     );
+// //   }
+// =======
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
 //       appBar: AppBar(
-//         title: Text('Levels'),
+//         title: const Text('Levels'),
 //         actions: [
 //           IconButton(
-//             icon: Icon(Icons.add),
+//             icon: const Icon(Icons.add),
 //             onPressed: () {
 //               // Add predefined data to levels
 //               _addPredefinedData();
@@ -57,13 +115,13 @@ class LevelsScreen extends StatefulWidget {
 //           // return
 //           Center(
 //         child: _isLoading
-//             ? CircularProgressIndicator()
+//             ? const CircularProgressIndicator()
 //             : _levels.isEmpty
-//                 ? Text('No Levels yet')
+//                 ? const Text('No Levels yet')
 //                 : ListView.builder(
 //                     itemCount: _levels.length,
 //                     itemBuilder: (context, index) {
-//                       UnitLevelsModel level = _levels[index];
+//                       LevelsModel level = _levels[index];
 //                       return ListTile(
 //                         title: Text(level.name),
 //                         subtitle: level.topicsuid.isNotEmpty
@@ -73,114 +131,56 @@ class LevelsScreen extends StatefulWidget {
 //                                     .map((topic) => Text(topic))
 //                                     .toList(),
 //                               )
-//                             : Text('No topics'),
+//                             : const Text('No topics'),
 //                       );
 //                     },
 //                   ),
 //       ),
 //       floatingActionButton: FloatingActionButton(
 //         onPressed: _fetchUsers,
-//         child: Icon(Icons.refresh),
+//         child: const Icon(Icons.refresh),
 //       ),
 //       // }
 //       // },
 //       // ),
 //     );
 //   }
-=======
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Levels'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              // Add predefined data to levels
-              _addPredefinedData();
-            },
-          ),
-        ],
-      ),
-      body:
-          // StreamBuilder<List<LevelsModel>>(
-          // stream: _levelsService.getAllLevelss(),
-          // builder: (context, snapshot) {
-          //   if (snapshot.connectionState == ConnectionState.waiting) {
-          //     return Center(child: CircularProgressIndicator());
-          //   } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          //     return Center(child: Text('No levels to show'));
-          //   } else {
-          // return
-          Center(
-        child: _isLoading
-            ? const CircularProgressIndicator()
-            : _levels.isEmpty
-                ? const Text('No Levels yet')
-                : ListView.builder(
-                    itemCount: _levels.length,
-                    itemBuilder: (context, index) {
-                      LevelsModel level = _levels[index];
-                      return ListTile(
-                        title: Text(level.name),
-                        subtitle: level.topicsuid.isNotEmpty
-                            ? Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: level.topicsuid
-                                    .map((topic) => Text(topic))
-                                    .toList(),
-                              )
-                            : const Text('No topics'),
-                      );
-                    },
-                  ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _fetchUsers,
-        child: const Icon(Icons.refresh),
-      ),
-      // }
-      // },
-      // ),
-    );
-  }
->>>>>>> 46dfe9e0f8ab133194e4615cc29ac3585a8d5804
+// >>>>>>> 46dfe9e0f8ab133194e4615cc29ac3585a8d5804
 
-//   void _fetchUsers() async {
-//     setState(() {
-//       _isLoading = true;
-//     });
+// //   void _fetchUsers() async {
+// //     setState(() {
+// //       _isLoading = true;
+// //     });
 
-//     try {
-//       final levels = await _levelsService.getAllLevelss();
-//       setState(() {
-//         _levels = levels;
-//         _isLoading = false;
-//       });
-//     } catch (e) {
-//       print('Failed to fetch levels: $e');
-//       setState(() {
-//         _isLoading = false;
-//       });
-//     }
-//   }
+// //     try {
+// //       final levels = await _levelsService.getAllLevelss();
+// //       setState(() {
+// //         _levels = levels;
+// //         _isLoading = false;
+// //       });
+// //     } catch (e) {
+// //       print('Failed to fetch levels: $e');
+// //       setState(() {
+// //         _isLoading = false;
+// //       });
+// //     }
+// //   }
 
-//   // Add predefined data to levels
-//   void _addPredefinedData() async {
-//     List<String> predefinedTopicsIds = ["First Id", "Second Id"];
+// //   // Add predefined data to levels
+// //   void _addPredefinedData() async {
+// //     List<String> predefinedTopicsIds = ["First Id", "Second Id"];
 
-//     UnitLevelsModel predefinedLevel = UnitLevelsModel(
-//       uid: '1',
-//       name: 'Updateeedddd',
-//       difficulty: 'Easy',
-//       order: 1,
-//       topicsuid: predefinedTopicsIds,
-//     );
-//     // await _levelsService.addLevels(Levels:  predefinedLevel);
-//     final x = await _levelsService.updateLevelsById(LevelsId: "9P4Ew8suBYcGtHTK5eVe",Levels: predefinedLevel);
-//     // print("the level get is ${x?.topicsId}");
+// //     UnitLevelsModel predefinedLevel = UnitLevelsModel(
+// //       uid: '1',
+// //       name: 'Updateeedddd',
+// //       difficulty: 'Easy',
+// //       order: 1,
+// //       topicsuid: predefinedTopicsIds,
+// //     );
+// //     // await _levelsService.addLevels(Levels:  predefinedLevel);
+// //     final x = await _levelsService.updateLevelsById(LevelsId: "9P4Ew8suBYcGtHTK5eVe",Levels: predefinedLevel);
+// //     // print("the level get is ${x?.topicsId}");
     
-//     _fetchUsers();
-//   }
-// }
+// //     _fetchUsers();
+// //   }
+// // }
--- codelingo/lib/screens/userlistscreen.dart
+++ codelingo/lib/screens/userlistscreen.dart
@@ -1,84 +1,84 @@
-import 'package:codelingo/models/UserModel.dart';
-import 'package:codelingo/services/UserService.dart';
-import 'package:flutter/material.dart';
+// import 'package:codelingo/models/UserModel.dart';
+// import 'package:codelingo/services/UserService.dart';
+// import 'package:flutter/material.dart';
 
-class UserListScreen extends StatefulWidget {
-  const UserListScreen({super.key});
+// class UserListScreen extends StatefulWidget {
+//   const UserListScreen({super.key});
 
-  @override
-  _UserListScreenState createState() => _UserListScreenState();
-}
+//   @override
+//   _UserListScreenState createState() => _UserListScreenState();
+// }
 
-class _UserListScreenState extends State<UserListScreen> {
-  final UserService _userService = UserService();
-  List<UserModel> _users = [];
-  bool _isLoading = false;
+// class _UserListScreenState extends State<UserListScreen> {
+//   final UserService _userService = UserService();
+//   List<UserModel> _users = [];
+//   bool _isLoading = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('User List'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: _addStaticUserData,
-          ),
-        ],
-      ),
-      body: Center(
-        child: _isLoading
-            ? const CircularProgressIndicator()
-            : _users.isEmpty
-                ? const Text('No users yet')
-                : ListView.builder(
-                    itemCount: _users.length,
-                    itemBuilder: (context, index) {
-                      final user = _users[index];
-                      return ListTile(
-                        title: Text(user.name),
-                        subtitle: Text('Age: ${user.name}'),
-                      );
-                    },
-                  ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _fetchUsers,
-        child: const Icon(Icons.refresh),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('User List'),
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.add),
+//             onPressed: _addStaticUserData,
+//           ),
+//         ],
+//       ),
+//       body: Center(
+//         child: _isLoading
+//             ? const CircularProgressIndicator()
+//             : _users.isEmpty
+//                 ? const Text('No users yet')
+//                 : ListView.builder(
+//                     itemCount: _users.length,
+//                     itemBuilder: (context, index) {
+//                       final user = _users[index];
+//                       return ListTile(
+//                         title: Text(user.name),
+//                         subtitle: Text('Age: ${user.name}'),
+//                       );
+//                     },
+//                   ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _fetchUsers,
+//         child: const Icon(Icons.refresh),
+//       ),
+//     );
+//   }
 
-  void _fetchUsers() async {
-    setState(() {
-      _isLoading = true;
-    });
+//   void _fetchUsers() async {
+//     setState(() {
+//       _isLoading = true;
+//     });
 
-    try {
-      final users = await _userService.getAllUsers();
-      setState(() {
-        _users = users;
-        _isLoading = false;
-      });
-    } catch (e) {
-      print('Failed to fetch users: $e');
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
+//     try {
+//       final users = await _userService.getAllUsers();
+//       setState(() {
+//         _users = users;
+//         _isLoading = false;
+//       });
+//     } catch (e) {
+//       print('Failed to fetch users: $e');
+//       setState(() {
+//         _isLoading = false;
+//       });
+//     }
+//   }
 
-  void _addStaticUserData() async {
-    try {
-      // Add static user data
-      await _userService.deleteUserById('LUfxI6FKggge6JVXsSbQ');
-      // print(x?.toJson());
-      // await _userService.addUser(UserModel(name: 'Bob', age: 30));
+//   void _addStaticUserData() async {
+//     try {
+//       // Add static user data
+//       await _userService.deleteUserById('LUfxI6FKggge6JVXsSbQ');
+//       // print(x?.toJson());
+//       // await _userService.addUser(UserModel(name: 'Bob', age: 30));
 
-      // Refresh user list
-      _fetchUsers();
-    } catch (e) {
-      print('Failed to add static user data: $e');
-    }
-  }
-}
+//       // Refresh user list
+//       _fetchUsers();
+//     } catch (e) {
+//       print('Failed to add static user data: $e');
+//     }
+//   }
+// }
