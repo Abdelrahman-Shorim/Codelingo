ec8d4cc2b6adfe8a78d4bb7caf5d29992526a73d
HabibaMohamed07 <Habiba2003520@miuegypt.edu.eg>
2024-06-09 03:47:16 +0300

    modifications

--- FrontEnd/lib/Screens/home.dart
+++ FrontEnd/lib/Screens/home.dart
@@ -2,19 +2,56 @@ import 'package:codelingo/Screens/course_screen/course_tree.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
-
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  final int _currentIndex =0;
+  final int _currentIndex = 0;
+
+  // Define completedLessons list here
+  List<List<bool>> completedLessons = [
+    // Unit 1
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 2
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 3
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 4
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 5
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 6
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 7
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 8
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 9
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 10
+    [false, false, false, false, false, false, false, false, false, false, false],
+    // Unit 11
+    [false, false, false, false, false, false, false, false, false, false, false],
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CourseTree(),
+    return Scaffold(
+      body: CourseTree(completedLessons: completedLessons),
     );
+  }
+
+  // Method to mark a lesson as completed
+  void completeLesson(int unitIndex, int lessonIndex) {
+    setState(() {
+      completedLessons[unitIndex][lessonIndex] = true;
+      // Automatically open the next lesson if it exists
+      if (lessonIndex + 1 < completedLessons[unitIndex].length) {
+        completedLessons[unitIndex][lessonIndex + 1] = true;
+      }
+    });
   }
 }
